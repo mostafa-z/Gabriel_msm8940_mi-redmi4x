ae22dff152222aeb7fbc10c91cfbab50f612b59d
mostafa-z <mostafazarghami@gmail.com>
2019-01-25 15:00:15 +0330

    kernel/fork: add missing part for threads-max observe limits
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -307,6 +307,9 @@ static void set_max_threads(unsigned int max_threads_suggested)
 		threads = div64_u64((u64) totalram_pages * (u64) PAGE_SIZE,
 				    (u64) THREAD_SIZE * 8UL);
 
+	if (threads > max_threads_suggested)
+		threads = max_threads_suggested;
+
 	max_threads = clamp_t(u64, threads, MIN_THREADS, MAX_THREADS);
 }
 
